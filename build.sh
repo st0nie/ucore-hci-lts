13bf51766086db48ce567fab759cc6d61ac6e844
ston <ston.jia@outlook.com>
2024-12-15 14:46:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 curl https://copr.fedorainfracloud.org/coprs/kwizart/kernel-longterm-6.6/repo/fedora-41/kwizart-kernel-longterm-6.6-fedora-41.repo -o /etc/yum.repos.d/kernel-lts.repo
+curl https://copr.fedorainfracloud.org/coprs/ganto/lxc4/repo/fedora-41/ganto-lxc4-fedora-41.repo -o /etc/yum.repos.d/incus.repo
 
 rpm-ostree cliwrap install-to-root /
 rpm-ostree override remove kernel kernel-{core,modules,modules-core} \
